ce01527a0f8ac5eded73fc6b94869823ef1c82d6
Ilya278-ai <pramestatus@gmail.com>
2025-02-05 21:06:15 +0700

    add html page for added company and controller and service and repository

--- src/main/java/org/example/employeeservice/model/Robot.java
+++ src/main/java/org/example/employeeservice/model/Robot.java
@@ -1,4 +1,14 @@
 package org.example.employeeservice.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
 public class Robot {
+    @Id
+    @GeneratedValue
+    private String name;
+    private double salaryInHour;
+
 }
