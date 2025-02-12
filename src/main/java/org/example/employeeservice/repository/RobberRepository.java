57929c1ef24883da7c52df562f9cf55a93b56b21
MikhailMazmanov <mihailmazmanov@gmail.com>
2025-02-12 16:49:29 +0300

    lesson_12_02_2025

--- src/main/java/org/example/employeeservice/repository/RobberRepository.java
+++ src/main/java/org/example/employeeservice/repository/RobberRepository.java
@@ -1,8 +1,9 @@
 package org.example.employeeservice.repository;
 
+import org.example.employeeservice.model.Robber;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RobberRepository  {
+public interface RobberRepository extends JpaRepository<Robber, Long>  {
 }
