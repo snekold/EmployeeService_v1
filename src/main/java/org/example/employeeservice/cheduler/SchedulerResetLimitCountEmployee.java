4ba3338641b178092c95450d1cc9ced3df1c545c
Ilya278-ai <pramestatus@gmail.com>
2025-02-19 22:09:08 +0700

    lesson 19_02_25

--- src/main/java/org/example/employeeservice/cheduler/SchedulerResetLimitCountEmployee.java
+++ src/main/java/org/example/employeeservice/cheduler/SchedulerResetLimitCountEmployee.java
@@ -2,10 +2,14 @@ package org.example.employeeservice.cheduler;
 
 
 
+import org.example.employeeservice.model.Company;
+import org.example.employeeservice.model.Employee;
 import org.example.employeeservice.service.CompanyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class SchedulerResetLimitCountEmployee {
@@ -13,8 +17,25 @@ public class SchedulerResetLimitCountEmployee {
     @Autowired
     private CompanyService companyService;
 
-    @Scheduled(cron = "0 * * * *  ?")
+    @Scheduled(fixedRate = 15000)
     public void reset(){
+        List<Company> allCompany = companyService.getAllCompany();
+
+        for (Company company : allCompany) {
+            List<Employee> employees = company.getEmployees();
+
+            int sumAllSalary = 0;
+            for (Employee employee : employees) {
+               sumAllSalary += employee.getSalary();
+
+            }
+            company.setBalance(company.getBalance() + sumAllSalary);
+
+            companyService.save(company);
+        }
+
+
+
     }
 
 
--- src/main/java/org/example/employeeservice/controller/EmployeeController.java
+++ src/main/java/org/example/employeeservice/controller/EmployeeController.java
@@ -6,6 +6,7 @@ import org.example.employeeservice.model.Company;
 import org.example.employeeservice.model.Employee;
 import org.example.employeeservice.repository.CompanyRepository;
 import org.example.employeeservice.repository.EmployeeRepository;
+import org.example.employeeservice.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -21,6 +22,9 @@ public class EmployeeController {
     @Autowired
     private EmployeeRepository employeeRepository;
 
+    @Autowired
+    private EmployeeService employeeService;
+
     @GetMapping("/employees/add")
     public String addEmployees() {
         return "add-employee";
@@ -35,18 +39,23 @@ public class EmployeeController {
                               Model model
 
     ) {
+        //добавление сотрудника в компанию
         System.out.println(name_company);
         Company company = companyRepository.findByName(name_company);
+
+        //проверка что компания not существует
         if (company == null) {
             model.addAttribute("message", "Company not found");
             return "add-employee";
         }
 
+        // check password company != password from input
         if (!password_company.equals(company.getPassword())) {
             model.addAttribute("message", "Password do not match");
             return "add-employee";
         }
 
+        // check count added employee this day
         if (company.getCountAddEmployeeThisDay() >= 3){
             model.addAttribute("message", "Вы добавили уже сегодня 3 сотрудников.Не нарушайте!");
             return "add-employee";
@@ -56,11 +65,11 @@ public class EmployeeController {
         Employee employee = new Employee();
         employee.setName(name);
         employee.setJobTitle(jobTitle);
-
+        employee.setSalary(employeeService.getRandomSalary());
         employee.setCompany(company);
 
         //прибавить 1 к добавленным сотрудникам за текущий день (нужно для проверки лимита 3 сотрудника в день)
-        company.setCountAddEmployeeThisDay(company.getCountAddEmployeeThisDay()+1);
+        company.plusCouny();
 
 
         companyRepository.save(company);
